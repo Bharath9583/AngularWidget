9fa4e18c5e552d294159816c0bc9b78caebcbb0c
Bharath9583 <bharathkumar.kamal@gmail.com>
2017-03-19 22:30:15 +0530

    Added 4 files

---
+++ js/controllers.js
@@ -0,0 +1,16 @@
+var currconvController = angular.module('myApp', [])
+ currconvController.controller('ConvertCtrl', ['$scope', '$http', function($scope, $http) {
+        $scope.rates = {};
+        $http.get('http://api.fixer.io/latest')
+            .then(function(res) {
+                $scope.rates = res.data.rates;
+                $scope.toType = $scope.rates.INR;
+                $scope.fromType = $scope.rates.USD;
+                $scope.fromValue = 1;
+                $scope.forExConvert();
+            });
+        $scope.forExConvert = function() {
+            $scope.toValue = $scope.fromValue * ($scope.toType * (1 / $scope.fromType));
+            $scope.toValue = $scope.toValue;
+        };
+    }]);
