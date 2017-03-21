66204e8258b4274ead51d65c8b6ea449ded43f94
Bharath9583 <bharathkumar.kamal@gmail.com>
2017-03-21 13:34:22 +0530

    Removed additional http get operation

--- js/controllers.js
+++ js/controllers.js
@@ -9,17 +9,12 @@ var currconvController = angular.module('myApp', [])
                 $scope.fromValue = 1;
                 $scope.forExConvert();
             });
-        $scope.forExConvert = function() {
-            $scope.rates = {};
-            $http.get('http://api.fixer.io/latest?base='+$scope.fromType)
-            .then(function(res) {
+        $scope.forExConvert = function() {            
                 if($scope.fromType === $scope.toType){
                     $scope.toValue = $scope.fromValue;
                 }else{
-                    $scope.rates = res.data.rates;
-                    $scope.toValue = $scope.fromValue * res.data.rates[$scope.toType];
+                    $scope.toValue = $scope.fromValue * ($scope.rates[$scope.toType] * (1 / $scope.rates[$scope.fromType]));
+                    $scope.toValue = Math.round($scope.toValue * 100) / 100;
                 }
-                
-        });
         };
     }]);
