40f5aa0f89d93c75ac81403697bcd337c23f01c9
Bharath9583 <bharathkumar.kamal@gmail.com>
2017-03-28 06:23:27 +0530

    Added default value and textboxvalidation

--- AngularExercise/js/AngJScontroller.js
+++ AngularExercise/js/AngJScontroller.js
@@ -1,4 +1,5 @@
 var converter = angular.module('ConverterApp', [])
+
 converter.controller('ConverterCntlr', ['$scope', '$http', function($scope, $http) {
 	$scope.rates = {};
 	$http.get('http://api.fixer.io/latest?base=ZAR')
@@ -6,7 +7,6 @@ converter.controller('ConverterCntlr', ['$scope', '$http', function($scope, $htt
 			$scope.rates = res.data.rates;
 			$scope.toCurType = "USD";
 			$scope.fromCurType = "CAD";
-			$scope.fromCurVal = 0.00;
 			$scope.toConvert();
 		});
 	$scope.toConvert = function() {            
@@ -17,4 +17,76 @@ converter.controller('ConverterCntlr', ['$scope', '$http', function($scope, $htt
 				$scope.toCurVal = Math.round($scope.toCurVal * 100) / 100;
 			}
 	};
+
+ }]);
+
+converter.directive('validNumber', function() {
+  return {
+    require: '?ngModel',
+    link: function(scope, element, attrs, ngModelCtrl) {
+      if(!ngModelCtrl) {
+        return; 
+      }
+
+      ngModelCtrl.$parsers.push(function(val) {
+        if (angular.isUndefined(val)) {
+            var val = '';
+        }
+        
+        var clean = val.replace(/[^-0-9\.]/g, '');
+        var negativeCheck = clean.split('-');
+		var decimalCheck = clean.split('.');
+        if(!angular.isUndefined(negativeCheck[1])) {
+            negativeCheck[1] = negativeCheck[1].slice(0, negativeCheck[1].length);
+            clean =negativeCheck[0] + '-' + negativeCheck[1];
+            if(negativeCheck[0].length > 0) {
+            	clean =negativeCheck[0];
+            }
+            
+        }
+          
+        if(!angular.isUndefined(decimalCheck[1])) {
+            decimalCheck[1] = decimalCheck[1].slice(0,2);
+            clean =decimalCheck[0] + '.' + decimalCheck[1];
+        }
+
+        if (val !== clean) {
+          ngModelCtrl.$setViewValue(clean);
+          ngModelCtrl.$render();
+        }
+        return clean;
+      });
+
+      element.bind('keypress', function(event) {
+        if(event.keyCode === 32) {
+          event.preventDefault();
+        }
+      });
+    }
+  };
+});
+
+converter.directive('myDirective', ['$window', function ($window) {
+
+     return {
+        link: link,
+        restrict: 'E',
+        template: '<div>window size: {{width}}px</div>'
+     };
+
+     function link(scope, element, attrs){
+
+       scope.width = $window.innerWidth;
+
+       angular.element($window).bind('resize', function(){
+
+         scope.width = $window.innerWidth;
+
+         // manuall $digest required as resize event
+         // is outside of angular
+         scope.$digest();
+       });
+
+     }
+
  }]);
